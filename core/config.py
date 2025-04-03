e4733b02daf7456b6df4e2bd7cfb351ea69b826c
Aldin Ahmethodzic <aldin.ahmethodzic@stu.ibu.edu.ba>
2025-04-04 00:38:15 +0200

    Chore: Project Setup
    - Vespa config setup
    - Chatbot logic implementation
    - Logger, config and requirements setup

---
+++ core/config.py
@@ -0,0 +1,3 @@
+GPT_MODEL = "gpt-4o"
+EMBEDDING_MODEL = "text-embedding-3-large"
+NUM_OF_CONTEXT_CHUNKS = 5
---
+++ core/settings.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+VESPA_HOST = os.environ.get("VESPA_HOST")
+VESPA_PORT = os.environ.get("VESPA_PORT")
