821f5bfd5a1871900150979847f7980698e52560
Aldin Ahmethodzic <aldin.ahmethodzic@stu.ibu.edu.ba>
2025-05-29 23:19:34 +0200

    Refactor: Message Schema

--- data_model/mongo_db/db.py
+++ data_model/mongo_db/db.py
@@ -2,9 +2,9 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 from core.settings import MONGO_URI, DATABASE_NAME
 from data_model.mongo_db.schemas.user import User
-from data_model.mongo_db.schemas.session import Session, Message
+from data_model.mongo_db.schemas.session import Session
 
 async def init_db():
     client = AsyncIOMotorClient(MONGO_URI)
     db = client[DATABASE_NAME]
-    await init_beanie(database=db, document_models=[User, Session, Message])
+    await init_beanie(database=db, document_models=[User, Session])
